8e50fd4ce601175ed57a932939ff37cd5d606ae5
mokhles95 <mokhleshaj@gmail.com>
2021-08-02 16:41:35 +0100

    category, marque, update and delete product  swal

--- src/app/backOffice/components/products/digital/digital-category/digital-category.component.ts
+++ src/app/backOffice/components/products/digital/digital-category/digital-category.component.ts
@@ -1,64 +1,61 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { digitalCategoryDB } from 'src/app/backOffice/shared/tables/digital-category';
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
-import { MatTableDataSource } from '@angular/material/table';
-import { Category } from 'src/app/models/category';
-import { CategoryService } from 'src/app/shared/services/category.service';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { FormControl, FormGroup } from '@angular/forms';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
-import { MatDialog } from '@angular/material/dialog';
-import { DialogContentExampleDialogComponent } from '../dialog-content-example-dialog/dialog-content-example-dialog.component';
-import { ModalUpdateComponent } from './modal-update/modal-update.component';
-import { MarqueService } from 'src/app/shared/services/marque.service';
-import { Marque } from 'src/app/models/marque';
-import { ModalUpdateMarqueComponent } from './modal-update-marque/modal-update-marque.component';
-
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { digitalCategoryDB } from "src/app/backOffice/shared/tables/digital-category";
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
+import { MatTableDataSource } from "@angular/material/table";
+import { Category } from "src/app/models/category";
+import { CategoryService } from "src/app/shared/services/category.service";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { FormControl, FormGroup } from "@angular/forms";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Router } from "@angular/router";
+import { MatDialog } from "@angular/material/dialog";
+import { DialogContentExampleDialogComponent } from "../dialog-content-example-dialog/dialog-content-example-dialog.component";
+import { ModalUpdateComponent } from "./modal-update/modal-update.component";
+import { MarqueService } from "src/app/shared/services/marque.service";
+import { Marque } from "src/app/models/marque";
+import { ModalUpdateMarqueComponent } from "./modal-update-marque/modal-update-marque.component";
+import Swal from "sweetalert2";
 @Component({
-  selector: 'app-digital-category',
-  templateUrl: './digital-category.component.html',
-  styleUrls: ['./digital-category.component.scss']
+  selector: "app-digital-category",
+  templateUrl: "./digital-category.component.html",
+  styleUrls: ["./digital-category.component.scss"],
 })
 export class DigitalCategoryComponent implements OnInit {
   dataSource: MatTableDataSource<Category>;
-  dataSourceMarque:MatTableDataSource<Marque>;
-  categories=[]
-  marques=[]
-  Category : Category
-  displayedColumns: string[] = [ 'name','buttons'];
-  displayedColumnsMarque: string[] = [ 'name','category','buttons'];
-  categoryForm:FormGroup
-  marqueFrom:FormGroup
+  dataSourceMarque: MatTableDataSource<Marque>;
+  categories = [];
+  marques = [];
+  Category: Category;
+  displayedColumns: string[] = ["name", "buttons"];
+  displayedColumnsMarque: string[] = ["name", "category", "buttons"];
+  categoryForm: FormGroup;
+  marqueFrom: FormGroup;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
-  constructor(private categoyService: CategoryService,
+  constructor(
+    private categoyService: CategoryService,
     private _snackBar: MatSnackBar,
-    private router : Router,
+    private router: Router,
     public dialog: MatDialog,
-    private marqueService:MarqueService) {
-      dialog.afterAllClosed
-    .subscribe(() => {
-    // update a variable or call a function when the dialog closes
+    private marqueService: MarqueService
+  ) {
+    dialog.afterAllClosed.subscribe(() => {
+      // update a variable or call a function when the dialog closes
       this.getCategories();
-    }
-  );
-   
+    });
   }
-
- 
 
   ngOnInit() {
     this.categoryForm = new FormGroup({
-      name: new FormControl()
+      name: new FormControl(),
     });
     this.marqueFrom = new FormGroup({
-      name:new FormControl(),
-      category: new FormControl()
-    })
-    this.getCategories()
-    this.getMarques()
+      name: new FormControl(),
+      category: new FormControl(),
+    });
+    this.getCategories();
+    this.getMarques();
   }
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
@@ -77,137 +74,156 @@ export class DigitalCategoryComponent implements OnInit {
       this.dataSourceMarque.paginator.firstPage();
     }
   }
-  
-  getCategories(){
-    this.categoyService.getCategories().subscribe(res=>{
-    this.categories = JSON.parse(JSON.stringify(res))
-    },
-    err=>{},
-    ()=>{
-      this.dataSource = new MatTableDataSource(this.categories);
-      this.dataSource.paginator = this.paginator;
-  this.dataSource.sort = this.sort;
-    })
+
+  getCategories() {
+    this.categoyService.getCategories().subscribe(
+      (res) => {
+        this.categories = JSON.parse(JSON.stringify(res));
+      },
+      (err) => {},
+      () => {
+        this.dataSource = new MatTableDataSource(this.categories);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      }
+    );
   }
-  addCategory(){
-    var data = this.categoryForm.getRawValue() 
+  addCategory() {
+    var data = this.categoryForm.getRawValue();
     this.categoyService.addCategory(data).subscribe(
-      res=>{
+      (res) => {
         console.log(res);
-        
       },
-      err=>{
+      (err) => {
         console.log(err);
-        
       },
-      ()=>{
-         // Show the success message
-         this._snackBar.open('une nouvelle categorie est ajouter avec succes', 'OK', {
-           verticalPosition: 'top',
-           duration        : 2000
-       });
-       this.getCategories()
-       this.categoryForm.reset();
-      
-      
+      () => {
+        // Show the success message
+        this._snackBar.open(
+          "une nouvelle categorie est ajouter avec succes",
+          "OK",
+          {
+            verticalPosition: "top",
+            duration: 2000,
+          }
+        );
+        this.getCategories();
+        this.categoryForm.reset();
       }
-    )
+    );
   }
-  addMarque(){
-    var data = this.marqueFrom.getRawValue() 
+  addMarque() {
+    var data = this.marqueFrom.getRawValue();
     this.marqueService.addMarque(data).subscribe(
-      res=>{
+      (res) => {
         console.log(res);
-        
       },
-      err=>{
+      (err) => {
         console.log(err);
-        
       },
-      ()=>{
-         // Show the success message
-         this._snackBar.open('une nouvelle categorie est ajouter avec succes', 'OK', {
-           verticalPosition: 'top',
-           duration        : 2000
-       });
-       this.getMarques()
-       this.marqueFrom.reset();
-      
-      
+      () => {
+        // Show the success message
+        this._snackBar.open(
+          "une nouvelle categorie est ajouter avec succes",
+          "OK",
+          {
+            verticalPosition: "top",
+            duration: 2000,
+          }
+        );
+        this.getMarques();
+        this.marqueFrom.reset();
       }
-    )
+    );
   }
-  getMarques(){
-    this.marqueService.getMarques().subscribe(res=>{
-      this.marques = JSON.parse(JSON.stringify(res))
+  getMarques() {
+    this.marqueService.getMarques().subscribe(
+      (res) => {
+        this.marques = JSON.parse(JSON.stringify(res));
       },
-      err=>{},
-      ()=>{
+      (err) => {},
+      () => {
         this.dataSourceMarque = new MatTableDataSource(this.marques);
         this.dataSourceMarque.paginator = this.paginator;
-    this.dataSourceMarque.sort = this.sort;
-      })
+        this.dataSourceMarque.sort = this.sort;
+      }
+    );
   }
-  delete(category){
-    const dialogRef = this.dialog.open(DialogContentExampleDialogComponent);
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result === "true"){
+  delete(category) {
+    Swal.fire({
+      title: "êtes-vous sûr de vouloir supprimer cette catégorie?",
+      text: "Vous ne serez pas en mesure de récupérer cette catégorie!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Oui",
+      cancelButtonText: "Non",
+    }).then((result) => {
+      if (result.isConfirmed) {
         this.categoyService.deleteCategory(category._id).subscribe(
-          result => {
+          (result) => {},
+          (e) => {
+            console.log(e);
           },
-          e => {console.log(e);
-           },
-    
-      ()=>{ 
-         // Show the success message
-         this._snackBar.open('une categorie est supprimé avec succes', 'OK', {
-          verticalPosition: 'top',
-          duration        : 2000
-      });
-      this.getCategories()
-      });
+
+          () => {
+            this.getCategories();
+            this.getMarques();
+          }
+        );
+        Swal.fire(
+          "catégorie supprimée!",
+          "cette catégorie a été sypprimer avec succes.",
+          "success"
+        );
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire("annulé", "Votre catégorie est en sécurité :)", "error");
       }
     });
   }
-  deleteMarque(marque){
-    const dialogRef = this.dialog.open(DialogContentExampleDialogComponent);
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result === "true"){
+  deleteMarque(marque) {
+    Swal.fire({
+      title: "êtes-vous sûr de vouloir supprimer cette marque?",
+      text: "Vous ne serez pas en mesure de récupérer cette marque!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Oui",
+      cancelButtonText: "Non",
+    }).then((result) => {
+      if (result.isConfirmed) {
         this.marqueService.deleteMarque(marque._id).subscribe(
-          result => {
+          (result) => {},
+          (e) => {
+            console.log(e);
           },
-          e => {console.log(e);
-           },
-    
-      ()=>{ 
-         // Show the success message
-         this._snackBar.open('une marque est supprimé avec succes', 'OK', {
-          verticalPosition: 'top',
-          duration        : 2000
-      });
-      this.getMarques()
-      });
+
+          () => {
+            this.getMarques();
+          }
+        );
+        Swal.fire(
+          "marque supprimée!",
+          "cette marque a été sypprimer avec succes.",
+          "success"
+        );
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire("annulé", "Votre marque est en sécurité :)", "error");
       }
     });
   }
-  update(category){
-    this.dialog.open(ModalUpdateComponent,{
-      width: '330px',
+  update(category) {
+    this.dialog.open(ModalUpdateComponent, {
+      width: "330px",
       data: {
-        dataKey: category
-      }});
-
+        dataKey: category,
+      },
+    });
   }
-  updateMarque(marque){
-    this.dialog.open(ModalUpdateMarqueComponent,{
-      width: '330px',
+  updateMarque(marque) {
+    this.dialog.open(ModalUpdateMarqueComponent, {
+      width: "330px",
       data: {
-        dataKey: marque
-      }});
+        dataKey: marque,
+      },
+    });
   }
-
 }
-
- 
--- src/app/backOffice/components/products/digital/update-product/update-product.component.ts
+++ src/app/backOffice/components/products/digital/update-product/update-product.component.ts
@@ -1,193 +1,190 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Artisant } from 'src/app/models/artisant';
-import { Category } from 'src/app/models/category';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Artisant } from "src/app/models/artisant";
+import { Category } from "src/app/models/category";
 // import { Collections } from 'src/app/models/collections';
-import { Marque } from 'src/app/models/marque';
-import { Product } from 'src/app/models/product';
-import { ArtisantService } from 'src/app/shared/services/artisant.service';
-import { CategoryService } from 'src/app/shared/services/category.service';
+import { Marque } from "src/app/models/marque";
+import { Product } from "src/app/models/product";
+import { ArtisantService } from "src/app/shared/services/artisant.service";
+import { CategoryService } from "src/app/shared/services/category.service";
 // import { CollectionService } from 'src/app/shared/services/collection.service';
-import { MarqueService } from 'src/app/shared/services/marque.service';
-import { ProductService } from 'src/app/shared/services/product.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MarqueService } from "src/app/shared/services/marque.service";
+import { ProductService } from "src/app/shared/services/product.service";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
-  selector: 'app-update-product',
-  templateUrl: './update-product.component.html',
-  styleUrls: ['./update-product.component.scss']
+  selector: "app-update-product",
+  templateUrl: "./update-product.component.html",
+  styleUrls: ["./update-product.component.scss"],
 })
 export class UpdateProductComponent implements OnInit {
   files: File[] = [];
-  product_id:string
-  product : any
-  productForm:FormGroup
+  product_id: string;
+  product: any;
+  productForm: FormGroup;
   categorie: Category;
- categories : any =  [];
- marque: Marque;
- marques = [];
- artisant : Artisant;
- artisantList= [];
- Oldimages=[];
- formData: FormData = new FormData();
-//  collection:Collections;
-//  collections= []
- showMarque:boolean = true;
- responseUpdate;
-  constructor(private route: ActivatedRoute,
+  categories: any = [];
+  marque: Marque;
+  marques = [];
+  artisant: Artisant;
+  artisantList = [];
+  Oldimages = [];
+  formData: FormData = new FormData();
+  //  collection:Collections;
+  //  collections= []
+  showMarque: boolean = true;
+  responseUpdate;
+  constructor(
+    private route: ActivatedRoute,
     private ps: ProductService,
     private _snackBar: MatSnackBar,
-    private fb :FormBuilder,
-    private cs: CategoryService, 
+    private fb: FormBuilder,
+    private cs: CategoryService,
     private ms: MarqueService,
-     private as:ArtisantService,
-     private router : Router
-    //  private collectionService: CollectionService,
-     ) {
-    
-   }
+    private as: ArtisantService,
+    private router: Router //  private collectionService: CollectionService,
+  ) {}
 
   ngOnInit(): void {
-    this.getArtisant()
-  this.getCategories()
-  // this.getCollections()
+    console.log("hello there");
+
+    this.getArtisant();
+    this.getCategories();
+    // this.getCollections()
     this.product_id = this.route.snapshot.params.id;
-    
-    this.ps.getProductById(this.product_id).subscribe(res=>{      
-      this.product=JSON.parse(JSON.stringify(res))
-     
+    console.log(this.product_id);
+
+    this.ps.getProductById(this.product_id).subscribe(
+      (res) => {
+        this.product = JSON.parse(JSON.stringify(res));
       },
-      err=>{},
-      ()=>{      
+      (err) => {},
+      () => {
         console.log(this.product);
-                
-        this.categorie = this.product.category
-     
-        this.createProductForm()})  
-   
+
+        this.categorie = this.product.category;
+
+        this.createProductForm();
+      }
+    );
   }
-  show(e){
-    if (e){
-        this.showMarque= false;
-        
-     this.ms.getMarqueByCategoryId(e).subscribe(res=>{
-         this.marques = JSON.parse(JSON.stringify(res))
-     },
-     error=>{
-         console.log(error);
-         
-     })
+  show(e) {
+    if (e) {
+      this.showMarque = false;
+
+      this.ms.getMarqueByCategoryId(e).subscribe(
+        (res) => {
+          this.marques = JSON.parse(JSON.stringify(res));
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
     }
- }
- getArtisant(){
-  this.as.getArtisant().subscribe(res=>{
-    this.artisantList = JSON.parse(JSON.stringify(res))
-  })
-   }
+  }
+  getArtisant() {
+    this.as.getArtisant().subscribe((res) => {
+      this.artisantList = JSON.parse(JSON.stringify(res));
+    });
+  }
   //  getCollections(){
   //    this.collectionService.getCollection().subscribe(result=>{
   // this.collections= JSON.parse(JSON.stringify(result))
   //    })
   //  }
-   getCategories(){
-    this.cs.getCategories().subscribe(result=>{
-      result.forEach(element=>{
-        this.categories.push(JSON.parse(JSON.stringify(element)))
-      })
-     
-       },
-       err=>{
-       },
-       ()=>{
-         this.categories.forEach(element => {
-           element.marque.forEach(marque => {
-             this.marques.push(marque);
-           });
-           
-         });
-       })
-   }
-  createProductForm()
-    {    
-         this.productForm = new FormGroup({
-          name: new FormControl(this.product.name),
-          price: new FormControl(this.product.price),
-          remise: new FormControl(this.product.remise),
-          reference: new FormControl(this.product.ref),
-          quantity: new FormControl(this.product.stock),
-          category: new FormControl(this.product.category),
-          marque: new FormControl(this.product.marque),
-          // collections: new FormControl(this.product.collections),
-          artisant: new FormControl(this.product.artisant)
+  getCategories() {
+    this.cs.getCategories().subscribe(
+      (result) => {
+        result.forEach((element) => {
+          this.categories.push(JSON.parse(JSON.stringify(element)));
         });
-       
-        this.Oldimages=this.product.images;
-    }
-    updateProduct(){
-      var data = this.productForm.getRawValue()  
-      console.log(data);
-      
-         
-       this.formData.set('name',data.name)
-       this.formData.set('prix',data.price)
-       this.formData.set('reference',data.reference)
-       this.formData.set('category',data.category)
-       this.formData.set('artisan',data.artisan)
-       this.formData.set('quantity',data.quantity)
-       this.formData.set('marque',data.marque)
-       this.formData.set('collections',data.collections)
-       this.formData.set('description',data.description)
-      // this.formData.set('new',data.new)
-       this.formData.set('remise',data.remise)
-       this.formData.set('oldImages',JSON.stringify(this.Oldimages))
-       let images=[];
-       this.files.forEach(element => {
-        images.push(element)
-       });
-      //  this.Oldimages.forEach(element => {
-      //   images.push(element)
-      //  });         
-       let fileCount = this.files.length;
-       if(fileCount>0 ){
-           for(let i=0;i<fileCount;i++){
-              this.formData.append('images',images[i]);
-           }
-       }
-       
-       console.log(this.formData);
-this.ps.updateProduct(this.route.snapshot.params.id,this.formData).subscribe(
-  res=>{this.responseUpdate=res;},
-  err=>{},
-  ()=>{
-    if(this.responseUpdate.message=="updated"){
-      this._snackBar.open('Produit modifié avec succès', 'OK', {
-        verticalPosition: 'top',
-        duration        : 2000
-    });
-    // // Change the location with new one
-    this.router.navigateByUrl('/products/product-list')
-    }
-    else {
-      this._snackBar.open('Réessayez', 'OK', {
-        verticalPosition: 'top',
-        duration        : 2000
+      },
+      (err) => {},
+      () => {
+        this.categories.forEach((element) => {
+          element.marque.forEach((marque) => {
+            this.marques.push(marque);
+          });
+        });
+      }
+    );
+  }
+  createProductForm() {
+    this.productForm = new FormGroup({
+      name: new FormControl(this.product.name),
+      price: new FormControl(this.product.price),
+      remise: new FormControl(this.product.remise),
+      reference: new FormControl(this.product.ref),
+      quantity: new FormControl(this.product.stock),
+      category: new FormControl(this.product.category),
+      marque: new FormControl(this.product.marque),
+      // collections: new FormControl(this.product.collections),
+      artisant: new FormControl(this.product.artisant),
     });
 
+    this.Oldimages = this.product.images;
+  }
+  updateProduct() {
+    var data = this.productForm.getRawValue();
+    this.formData.set("name", data.name);
+    this.formData.set("prix", data.price);
+    this.formData.set("reference", data.reference);
+    this.formData.set("category", data.category);
+    this.formData.set("artisan", data.artisan);
+    this.formData.set("quantity", data.quantity);
+    this.formData.set("marque", data.marque);
+    this.formData.set("description", data.description);
+    this.formData.set("remise", data.remise);
+    this.formData.set("oldImages", JSON.stringify(this.Oldimages));
+    let images = [];
+    this.files.forEach((element) => {
+      images.push(element);
+    });
+    //  this.Oldimages.forEach(element => {
+    //   images.push(element)
+    //  });
+    let fileCount = this.files.length;
+    if (fileCount > 0) {
+      for (let i = 0; i < fileCount; i++) {
+        this.formData.append("images", images[i]);
+      }
     }
+
+    console.log(this.formData);
+    this.ps
+      .updateProduct(this.route.snapshot.params.id, this.formData)
+      .subscribe(
+        (res) => {
+          this.responseUpdate = res;
+        },
+        (err) => {},
+        () => {
+          if (this.responseUpdate.message == "updated") {
+            this._snackBar.open("Produit modifié avec succès", "OK", {
+              verticalPosition: "top",
+              duration: 2000,
+            });
+            // // Change the location with new one
+            this.router.navigateByUrl("/products/product-list");
+          } else {
+            this._snackBar.open("Réessayez", "OK", {
+              verticalPosition: "top",
+              duration: 2000,
+            });
+          }
+        }
+      );
   }
-)
+  close(i) {
+    const index = this.Oldimages.indexOf(i);
+    this.Oldimages.splice(index, 1);
   }
-  close(i){
-    const index = this.Oldimages.indexOf(i)
-    this.Oldimages.splice(index,1)
-}
   onSelect(event) {
-     
     this.files.push(...event.addedFiles);
     console.log(this.files);
   }
-   onRemove(event) {
+  onRemove(event) {
     this.files.splice(this.files.indexOf(event), 1);
   }
 }
