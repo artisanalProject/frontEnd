2ce248df7821bdd63940a081edb36acd2960982a
mokhles95 <mokhleshaj@gmail.com>
2021-08-03 18:09:44 +0100

    contact details and change status

--- src/app/backOffice/components/setting/message/message.component.ts
+++ src/app/backOffice/components/setting/message/message.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { ContactService } from "src/app/shared/services/contact.service";
 
 @Component({
-  selector: 'app-message',
-  templateUrl: './message.component.html',
-  styleUrls: ['./message.component.scss']
+  selector: "app-message",
+  templateUrl: "./message.component.html",
+  styleUrls: ["./message.component.scss"],
 })
 export class MessageComponent implements OnInit {
-
-  constructor() { }
+  id: String;
+  message = "";
+  constructor(private route: ActivatedRoute, private cs: ContactService) {}
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params.id;
+    this.cs.getContactById(this.id).subscribe(
+      (res) => {
+        this.message = JSON.parse(JSON.stringify(res));
+      },
+      (err) => {
+        console.log(err);
+      },
+      () => {
+        this.cs.changeStatus(this.id).subscribe((res) => {
+          console.log(res);
+        });
+      }
+    );
   }
-
 }
--- src/app/shared/services/contact.service.ts
+++ src/app/shared/services/contact.service.ts
@@ -1,30 +1,47 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { Contact } from 'src/app/models/contact';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, BehaviorSubject } from "rxjs";
+import { Contact } from "src/app/models/contact";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ContactService {
   public notifEmails = new BehaviorSubject(0);
   nbNotif;
-  baseUrl="/api";
-  constructor(private http: HttpClient) { }
+  baseUrl = "/api";
+  constructor(private http: HttpClient) {}
 
   public addContact(contact): Observable<Contact[]> {
-    return this.http.post<Contact[]>(this.baseUrl+"/contact/addContact",contact)
+    return this.http.post<Contact[]>(
+      this.baseUrl + "/contact/addContact",
+      contact
+    );
   }
   public getContact(): Observable<Contact[]> {
-    return this.http.get<Contact[]>(this.baseUrl+"/contact/getContact")
+    return this.http.get<Contact[]>(this.baseUrl + "/contact/getContact");
+  }
+  public getContactById(id): Observable<Contact> {
+    return this.http.get<Contact>(
+      this.baseUrl + "/contact/getContactById/" + id
+    );
   }
 
-  public  nbNotifEmails() {
-      this.getContact().subscribe(res=>{
-        this.nbNotif=JSON.parse(JSON.stringify(res)).filter(x=>x.status=="unread").length
-      },err=>{},()=>{
-        this.notifEmails.next(this.nbNotif)
-      })
-     }
-  
+  public changeStatus(id) {
+    return this.http.get(this.baseUrl + "/contact/changeStatus/" + id);
+  }
+
+  public nbNotifEmails() {
+    this.getContact().subscribe(
+      (res) => {
+        this.nbNotif = JSON.parse(JSON.stringify(res)).filter(
+          (x) => x.status == "unread"
+        ).length;
+      },
+      (err) => {},
+      () => {
+        this.notifEmails.next(this.nbNotif);
+      }
+    );
+  }
 }
