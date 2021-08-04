ff2bfbd4a3fd9a833d77d3de5c8648171fc048ef
mokhles95 <mokhleshaj@gmail.com>
2021-08-04 12:41:51 +0100

    some changes in front-office

--- src/app/backOffice/components/auth/login/login.component.ts
+++ src/app/backOffice/components/auth/login/login.component.ts
@@ -1,26 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Artisant } from 'src/app/models/artisant';
-import { artisanService } from 'src/app/services/artisanService';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { adminService } from 'src/app/services/adminService';
-import { Router } from '@angular/router';
- 
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Artisant } from "src/app/models/artisant";
+import { artisanService } from "src/app/services/artisanService";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { adminService } from "src/app/services/adminService";
+import { Router } from "@angular/router";
+
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
-
-  
   public loginForm: FormGroup;
   public registerForm: FormGroup;
   responseRegister;
   responseLogin;
   responseForgotPwd;
 
-  constructor(private formBuilder: FormBuilder, private artisanService : artisanService,private _snackBar: MatSnackBar,private adminService : adminService,private router : Router) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private artisanService: artisanService,
+    private _snackBar: MatSnackBar,
+    private adminService: adminService,
+    private router: Router
+  ) {
     this.createLoginForm();
     this.createRegisterForm();
   }
@@ -28,169 +32,191 @@ export class LoginComponent implements OnInit {
   owlcarousel = [
     {
       title: "Welcome to Multikart",
-      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.",
     },
     {
       title: "Welcome to Multikart",
-      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.",
     },
     {
       title: "Welcome to Multikart",
-      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.",
-    }
-  ]
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.",
+    },
+  ];
   owlcarouselOptions = {
     loop: true,
     items: 1,
-    dots: true
+    dots: true,
   };
 
   createLoginForm() {
     this.loginForm = this.formBuilder.group({
-      email: ['',[Validators.required, Validators.email]],
-      password: ['',Validators.required],
-      checkAdmin:[false]
-    })
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", Validators.required],
+      checkAdmin: [false],
+    });
   }
   createRegisterForm() {
     this.registerForm = this.formBuilder.group({
-      name: ['',Validators.required],
-      email: ['',[Validators.required, Validators.email]],
-      password: ['',Validators.required],
-      phoneNumber: ['',Validators.required],
-      address:['',Validators.required],
-      storeName: ['',Validators.required],
-      typeOfWork: ['',Validators.required],
-      codePostal: ['',Validators.required],
-      cin: ['',Validators.required],
-    })
+      name: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", Validators.required],
+      phoneNumber: ["", Validators.required],
+      address: ["", Validators.required],
+      storeName: ["", Validators.required],
+      typeOfWork: ["", Validators.required],
+      codePostal: ["", Validators.required],
+      cin: ["", Validators.required],
+    });
   }
 
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
-  login(){
-    if(this.loginForm.invalid){
-      this._snackBar.open('Tous les champs sont obligatoires','ok',{
+  login() {
+    if (this.loginForm.invalid) {
+      this._snackBar.open("Tous les champs sont obligatoires", "ok", {
         duration: 5000,
-        verticalPosition: 'top',
-        horizontalPosition: 'center',
+        verticalPosition: "top",
+        horizontalPosition: "center",
       });
-    }
-    else if(!this.loginForm.controls.checkAdmin.value){
-      this.artisanService.login(this.loginForm.value).subscribe(res=>{
-      this.responseLogin=JSON.parse(JSON.stringify(res));
-      },
-      err=>{console.log(err);
-      },()=>{
-        if(this.responseLogin=="verify email or password"){
-          this._snackBar.open('Vérifiez email ou mot de passe','ok',{
-            duration: 10000,
-            verticalPosition: 'top',
-            horizontalPosition: 'center',
-          });
+    } else if (!this.loginForm.controls.checkAdmin.value) {
+      this.artisanService.login(this.loginForm.value).subscribe(
+        (res) => {
+          this.responseLogin = JSON.parse(JSON.stringify(res));
+        },
+        (err) => {
+          console.log(err);
+        },
+        () => {
+          if (this.responseLogin == "verify email or password") {
+            this._snackBar.open("Vérifiez email ou mot de passe", "ok", {
+              duration: 10000,
+              verticalPosition: "top",
+              horizontalPosition: "center",
+            });
+          } else if (this.responseLogin == "not activated") {
+            this._snackBar.open(
+              "Votre compte n'est pas activé. Vous recevrez un mail d'activation le plutôt possible",
+              "ok",
+              {
+                duration: 10000,
+                verticalPosition: "top",
+                horizontalPosition: "center",
+              }
+            );
+          } else {
+            localStorage.setItem(
+              "connectedUser",
+              JSON.stringify(this.responseLogin)
+            );
+            console.log("artisaaan " + this.responseLogin);
+            this.router.navigateByUrl("/admin/dashboard/default");
+          }
         }
-        else if(this.responseLogin=="not activated"){
-          this._snackBar.open("Votre compte n'est pas activé. Vous recevrez un mail d'activation le plutôt possible",'ok',{
-            duration: 10000,
-            verticalPosition: 'top',
-            horizontalPosition: 'center',
-          });
+      );
+    } else {
+      this.adminService.login(this.loginForm.value).subscribe(
+        (res) => {
+          this.responseLogin = JSON.parse(JSON.stringify(res));
+        },
+        (err) => {},
+        () => {
+          console.log(this.responseLogin);
+
+          if (this.responseLogin == "verify email or password") {
+            this._snackBar.open("Vérifiez email ou mot de passe", "ok", {
+              duration: 10000,
+              verticalPosition: "top",
+              horizontalPosition: "center",
+            });
+          } else {
+            localStorage.setItem(
+              "connectedUser",
+              JSON.stringify(this.responseLogin)
+            );
+            this.router.navigateByUrl("/admin/dashboard/default");
+            console.log("admiin " + this.responseLogin);
+          }
         }
-        else {
-          localStorage.setItem('connectedUser',JSON.stringify(this.responseLogin))
-          console.log("artisaaan "+this.responseLogin);
-          this.router.navigateByUrl('/admin/dashboard/default');
-        }
-      })
-    }
-    else {
-      console.log("xxx");
-      
-      this.adminService.login(this.loginForm.value).subscribe(res=>{this.responseLogin=JSON.parse(JSON.stringify(res))},
-      err=>{},
-      ()=>{
-        console.log(this.responseLogin);
-        
-        if(this.responseLogin=="verify email or password"){
-          this._snackBar.open('Vérifiez email ou mot de passe','ok',{
-            duration: 10000,
-            verticalPosition: 'top',
-            horizontalPosition: 'center',
-          });
-        }
-        else{
-          localStorage.setItem('connectedUser',JSON.stringify(this.responseLogin));
-          this.router.navigateByUrl('/admin/dashboard/default');
-          console.log("admiin "+this.responseLogin);
-        }
-      })
+      );
     }
   }
-  register(){
-    if(this.registerForm.invalid){
-      this._snackBar.open('Tous les champs sont obligatoires','ok',{
+  register() {
+    if (this.registerForm.invalid) {
+      this._snackBar.open("Tous les champs sont obligatoires", "ok", {
         duration: 5000,
-        verticalPosition: 'top',
-        horizontalPosition: 'center',
+        verticalPosition: "top",
+        horizontalPosition: "center",
+      });
+    } else {
+      this.artisanService.register(this.registerForm.value).subscribe(
+        (res) => {
+          this.responseRegister = JSON.parse(JSON.stringify(res));
+        },
+        (err) => {
+          console.log(err);
+        },
+        () => {
+          if (this.responseRegister == "account already exist") {
+            this._snackBar.open("Vous avez déjà un compte", "ok", {
+              duration: 10000,
+              verticalPosition: "top",
+              horizontalPosition: "center",
+            });
+          } else {
+            this._snackBar.open(
+              "Vous êtes inscri avec succès, vous recevrez un email de confirmation le plutôt possible",
+              "ok",
+              {
+                duration: 10000,
+                verticalPosition: "top",
+                horizontalPosition: "center",
+              }
+            );
+          }
+        }
+      );
+    }
+  }
+
+  forgotPwd() {
+    if (this.loginForm.controls.email.value) {
+      this.adminService
+        .forgotPwd(this.loginForm.controls.email.value)
+        .subscribe(
+          (res) => {
+            this.responseForgotPwd = JSON.parse(JSON.stringify(res));
+          },
+          (err) => {},
+          () => {
+            if (this.responseForgotPwd == "Email sent!") {
+              this._snackBar.open(
+                "Un email de récupération de mot de passe est envoyé",
+                "ok",
+                {
+                  duration: 5000,
+                  verticalPosition: "top",
+                  horizontalPosition: "center",
+                }
+              );
+            } else {
+              this._snackBar.open("Une erreur s'est produite !", "ok", {
+                duration: 10000,
+                verticalPosition: "top",
+                horizontalPosition: "center",
+              });
+            }
+          }
+        );
+    } else {
+      this._snackBar.open("Veuillez entrer votre adresse mail", "ok", {
+        duration: 10000,
+        verticalPosition: "top",
+        horizontalPosition: "center",
       });
     }
-    else{
-  this.artisanService.register(this.registerForm.value).subscribe(res=>{this.responseRegister=JSON.parse(JSON.stringify(res))},err=>{
-    console.log(err);
-    
-  },()=>{
-    if(this.responseRegister=="account already exist"){
-      this._snackBar.open('Vous avez déjà un compte','ok',{
-        duration: 10000,
-        verticalPosition: 'top',
-        horizontalPosition: 'center',
-      });
-    }else{
-      this._snackBar.open('Vous êtes inscri avec succès, vous recevrez un email de confirmation le plutôt possible','ok',{
-        duration: 10000,
-        verticalPosition: 'top',
-        horizontalPosition: 'center',
-      });
-    }
-    
-  })
-    }
- }
-
-
- forgotPwd(){
-   if(this.loginForm.controls.email.value){
-    this.adminService.forgotPwd(this.loginForm.controls.email.value).subscribe(res=>{this.responseForgotPwd=JSON.parse(JSON.stringify(res))},
-    err=>{},
-    ()=>{
-      if(this.responseForgotPwd=="Email sent!"){
-        this._snackBar.open('Un email de récupération de mot de passe est envoyé','ok',{
-          duration: 5000,
-          verticalPosition: 'top',
-          horizontalPosition: 'center',
-        });
-      }else{
-        this._snackBar.open("Une erreur s'est produite !",'ok',{
-          duration: 10000,
-          verticalPosition: 'top',
-          horizontalPosition: 'center',
-        });
-      }
-    })
-   }
-    else{
-      this._snackBar.open('Veuillez entrer votre adresse mail','ok',{
-        duration: 10000,
-        verticalPosition: 'top',
-        horizontalPosition: 'center',
-      });
-    } 
-
-    
-   
-   
- }
-
+  }
 }
--- src/app/shop/collection/collection-left-sidebar/collection-left-sidebar.component.ts
+++ src/app/shop/collection/collection-left-sidebar/collection-left-sidebar.component.ts
@@ -1,19 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ViewportScroller } from '@angular/common';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ViewportScroller } from "@angular/common";
 import { ProductService } from "../../../shared/services/product.service";
-import { Product } from '../../../models/product';
-import { Category } from 'src/app/models/category';
+import { Product } from "../../../models/product";
+import { Category } from "src/app/models/category";
 
 @Component({
-  selector: 'app-collection-left-sidebar',
-  templateUrl: './collection-left-sidebar.component.html',
-  styleUrls: ['./collection-left-sidebar.component.scss']
+  selector: "app-collection-left-sidebar",
+  templateUrl: "./collection-left-sidebar.component.html",
+  styleUrls: ["./collection-left-sidebar.component.scss"],
 })
 export class CollectionLeftSidebarComponent implements OnInit {
-  
-  public grid: string = 'col-xl-3 col-md-6';
-  public layoutView: string = 'grid-view';
+  public grid: string = "col-xl-3 col-md-6";
+  public layoutView: string = "grid-view";
   public products: Product[] = [];
   public brands: any[] = [];
   public colors: any[] = [];
@@ -22,8 +21,8 @@ export class CollectionLeftSidebarComponent implements OnInit {
   public maxPrice: number = 1200;
   public tags: any[] = [];
   public category: string;
-  public marque : string;
-  public artisant : string;
+  public marque: string;
+  public artisant: string;
 
   public pageNo: number = 1;
   public paginate: any = {}; // Pagination use only
@@ -31,138 +30,166 @@ export class CollectionLeftSidebarComponent implements OnInit {
   public mobileSidebar: boolean = false;
   public loader: boolean = true;
 
-  constructor(private route: ActivatedRoute, private router: Router,
-    private viewScroller: ViewportScroller, public productService: ProductService) {   
-     
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private viewScroller: ViewportScroller,
+    public productService: ProductService
+  ) {}
 
   ngOnInit(): void {
-    this.productService.getProducts().subscribe(products=>{
-      this.products = products
-    },err=>{},()=> 
-     // Get Query params..
-     this.route.params.subscribe(params => {
+    this.productService.getProducts().subscribe(
+      (products) => {
+        this.products = products;
+      },
+      (err) => {},
+      () =>
+        // Get Query params..
+        this.route.params.subscribe((params) => {
+          this.brands = params.brand ? params.brand.split(",") : [];
+          this.colors = params.color ? params.color.split(",") : [];
+          this.size = params.size ? params.size.split(",") : [];
+          this.minPrice = params.minPrice ? params.minPrice : this.minPrice;
+          this.maxPrice = params.maxPrice ? params.maxPrice : this.maxPrice;
+          this.tags = [...this.brands, ...this.colors, ...this.size]; // All Tags Array
 
-      this.brands = params.brand ? params.brand.split(",") : [];
-      this.colors = params.color ? params.color.split(",") : [];
-      this.size  = params.size ? params.size.split(",")  : [];
-      this.minPrice = params.minPrice ? params.minPrice : this.minPrice;
-      this.maxPrice = params.maxPrice ? params.maxPrice : this.maxPrice;
-      this.tags = [...this.brands, ...this.colors, ...this.size]; // All Tags Array
-      
-      this.category = params.category ? params.category : null;
-      this.sortBy = params.sortBy ? params.sortBy : 'ascending';
-      this.pageNo = params.page ? params.page : this.pageNo;
-      this.marque = params.marque ? params.marque : null;
-      console.log(this.marque);
-      console.log(this.category);
-      
-      this.artisant = params.artisant ? params.artisant : null;
-      // Get Filtered Products..
-      this.productService.filterProducts(this.tags).subscribe(response => {         
-        // Sorting Filter
-        this.products = this.productService.sortProducts(response, this.sortBy);
-        // Category Filter
-        if(params.category)
-          this.products = this.products.filter(item => item.category.name == this.category);
-         
-          // filter marque
-          if(params.marque)
-          this.products = this.products.filter(item => item.marque.name == this.marque);
-          console.log(this.products);
-          // filter artisant
-          if(params.artisant)
-          this.products = this.products.filter(item => item.artisant.name == this.artisant);
-         
-         
-        // Price Filter
-        this.products = this.products.filter(item => item.price >= this.minPrice && item.price <= this.maxPrice) 
-        // Paginate Products
-        this.paginate = this.productService.getPager(this.products.length, +this.pageNo);     // get paginate object from service
-        this.products = this.products.slice(this.paginate.startIndex, this.paginate.endIndex + 1); // get current page of items
-      })
-    })
+          this.category = params.category ? params.category : null;
+          this.sortBy = params.sortBy ? params.sortBy : "ascending";
+          this.pageNo = params.page ? params.page : this.pageNo;
+          this.marque = params.marque ? params.marque : null;
+
+          this.artisant = params.artisant ? params.artisant : null;
+          // Get Filtered Products..
+          this.productService
+            .filterProducts(this.tags)
+            .subscribe((response) => {
+              // Sorting Filter
+              this.products = this.productService.sortProducts(
+                response,
+                this.sortBy
+              );
+              // Category Filter
+              if (params.category)
+                this.products = this.products.filter(
+                  (item) => item.category.name == this.category
+                );
+
+              // filter marque
+              if (params.marque)
+                this.products = this.products.filter(
+                  (item) => item.marque.name == this.marque
+                );
+              console.log(this.products);
+              // filter artisant
+              if (params.artisant)
+                this.products = this.products.filter(
+                  (item) => item.artisant.name == this.artisant
+                );
+
+              // Price Filter
+              this.products = this.products.filter(
+                (item) =>
+                  item.price >= this.minPrice && item.price <= this.maxPrice
+              );
+              // Paginate Products
+              this.paginate = this.productService.getPager(
+                this.products.length,
+                +this.pageNo
+              ); // get paginate object from service
+              this.products = this.products.slice(
+                this.paginate.startIndex,
+                this.paginate.endIndex + 1
+              ); // get current page of items
+            });
+        })
     );
-    
-  ;
-    
   }
-
 
   // Append filter value to Url
   updateFilter(tags: any) {
+    console.log(tags);
+
     tags.page = null; // Reset Pagination
-    this.router.navigate([], { 
-      relativeTo: this.route,
-      queryParams: tags,
-      queryParamsHandling: 'merge', // preserve the existing query params in the route
-      skipLocationChange: false  // do trigger navigation
-    }).finally(() => {
-      this.viewScroller.setOffset([120, 120]);
-      this.viewScroller.scrollToAnchor('products'); // Anchore Link
-    });
+    this.router
+      .navigate([], {
+        relativeTo: this.route,
+        queryParams: tags,
+        queryParamsHandling: "merge", // preserve the existing query params in the route
+        skipLocationChange: false, // do trigger navigation
+      })
+      .finally(() => {
+        this.viewScroller.setOffset([120, 120]);
+        this.viewScroller.scrollToAnchor("products"); // Anchore Link
+      });
   }
 
   // SortBy Filter
   sortByFilter(value) {
-    this.router.navigate([], { 
-      relativeTo: this.route,
-      queryParams: { sortBy: value ? value : null},
-      queryParamsHandling: 'merge', // preserve the existing query params in the route
-      skipLocationChange: false  // do trigger navigation
-    }).finally(() => {
-      this.viewScroller.setOffset([120, 120]);
-      this.viewScroller.scrollToAnchor('products'); // Anchore Link
-    });
+    this.router
+      .navigate([], {
+        relativeTo: this.route,
+        queryParams: { sortBy: value ? value : null },
+        queryParamsHandling: "merge", // preserve the existing query params in the route
+        skipLocationChange: false, // do trigger navigation
+      })
+      .finally(() => {
+        this.viewScroller.setOffset([120, 120]);
+        this.viewScroller.scrollToAnchor("products"); // Anchore Link
+      });
   }
 
   // Remove Tag
   removeTag(tag) {
-  
-    this.brands = this.brands.filter(val => val !== tag);
-    this.colors = this.colors.filter(val => val !== tag);
-    this.size = this.size.filter(val => val !== tag );
+    this.brands = this.brands.filter((val) => val !== tag);
+    this.colors = this.colors.filter((val) => val !== tag);
+    this.size = this.size.filter((val) => val !== tag);
 
-    let params = { 
-      brand: this.brands.length ? this.brands.join(",") : null, 
-      color: this.colors.length ? this.colors.join(",") : null, 
-      size: this.size.length ? this.size.join(",") : null
-    }
+    let params = {
+      brand: this.brands.length ? this.brands.join(",") : null,
+      color: this.colors.length ? this.colors.join(",") : null,
+      size: this.size.length ? this.size.join(",") : null,
+    };
 
-    this.router.navigate([], { 
-      relativeTo: this.route,
-      queryParams: params,
-      queryParamsHandling: 'merge', // preserve the existing query params in the route
-      skipLocationChange: false  // do trigger navigation
-    }).finally(() => {
-      this.viewScroller.setOffset([120, 120]);
-      this.viewScroller.scrollToAnchor('products'); // Anchore Link
-    });
+    this.router
+      .navigate([], {
+        relativeTo: this.route,
+        queryParams: params,
+        queryParamsHandling: "merge", // preserve the existing query params in the route
+        skipLocationChange: false, // do trigger navigation
+      })
+      .finally(() => {
+        this.viewScroller.setOffset([120, 120]);
+        this.viewScroller.scrollToAnchor("products"); // Anchore Link
+      });
   }
 
   // Clear Tags
   removeAllTags() {
-    this.router.navigate([], { 
-      relativeTo: this.route,
-      queryParams: {},
-      skipLocationChange: false  // do trigger navigation
-    }).finally(() => {
-      this.viewScroller.setOffset([120, 120]);
-      this.viewScroller.scrollToAnchor('products'); // Anchore Link
-    });
+    this.router
+      .navigate([], {
+        relativeTo: this.route,
+        queryParams: {},
+        skipLocationChange: false, // do trigger navigation
+      })
+      .finally(() => {
+        this.viewScroller.setOffset([120, 120]);
+        this.viewScroller.scrollToAnchor("products"); // Anchore Link
+      });
   }
 
   // product Pagination
   setPage(page: number) {
-    this.router.navigate([], { 
-      relativeTo: this.route,
-      queryParams: { page: page },
-      queryParamsHandling: 'merge', // preserve the existing query params in the route
-      skipLocationChange: false  // do trigger navigation
-    }).finally(() => {
-      this.viewScroller.setOffset([120, 120]);
-      this.viewScroller.scrollToAnchor('products'); // Anchore Link
-    });
+    this.router
+      .navigate([], {
+        relativeTo: this.route,
+        queryParams: { page: page },
+        queryParamsHandling: "merge", // preserve the existing query params in the route
+        skipLocationChange: false, // do trigger navigation
+      })
+      .finally(() => {
+        this.viewScroller.setOffset([120, 120]);
+        this.viewScroller.scrollToAnchor("products"); // Anchore Link
+      });
   }
 
   // Change Grid Layout
@@ -173,15 +200,12 @@ export class CollectionLeftSidebarComponent implements OnInit {
   // Change Layout View
   updateLayoutView(value: string) {
     this.layoutView = value;
-    if(value == 'list-view')
-      this.grid = 'col-lg-12';
-    else
-      this.grid = 'col-xl-3 col-md-6';
+    if (value == "list-view") this.grid = "col-lg-12";
+    else this.grid = "col-xl-3 col-md-6";
   }
 
   // Mobile sidebar
   toggleMobileSidebar() {
     this.mobileSidebar = !this.mobileSidebar;
   }
-
 }
