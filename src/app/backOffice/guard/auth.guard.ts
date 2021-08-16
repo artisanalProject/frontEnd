e04cdf42a14069691ffef8d81de09c80bf17281a
mokhles95 <mokhleshaj@gmail.com>
2021-08-16 18:03:40 +0100

    blog routing

--- src/app/backOffice/guard/auth.guard.ts
+++ src/app/backOffice/guard/auth.guard.ts
@@ -11,14 +11,16 @@ export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private route: Router){
 
   }
+  token : any
   canActivate(){
+   this.token = JSON.parse(JSON.stringify(this.authService.loggedIn()))
+    console.log(this.token);
+    
     if (this.authService.loggedIn()){      
       return true
     }
     else {
       this.route.navigate(['/auth/login'])
-  
-      
       return false
     }
   }
